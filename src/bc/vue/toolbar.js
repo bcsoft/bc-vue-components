e0d5a08564c6756200d21e6491b3ad87bd178176
dragon <rongjihuang@gmail.com>
2016-07-04 10:44:12 +0800

    add toolbar component

---
+++ src/bc/vue/toolbar.js
@@ -0,0 +1,19 @@
+/**
+ * 工具条组件
+ * <pre>
+ *   UI 使用：
+ *   <bc-toolbar [@click="yourMethodName"]>
+ *     ...[slot content]
+ *   </bc-toolbar>
+ * </pre>
+ */
+define(['vue', "css!bc/vue/toolbar"], function (Vue) {
+  'use strict';
+  return Vue.component('bc-toolbar', {
+    template: '<div class="bc-vue-toolbar ui-widget-content" style="">' +
+    '<slot></slot>' +
+    '</div>',
+    replace: true,
+    props: {}
+  });
+});
