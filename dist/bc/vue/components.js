470d75a7c272f34378cab7b32f0ef230a69e7a16
dragon <rongjihuang@gmail.com>
2016-08-15 14:55:06 +0800

    release v0.2.0

--- dist/bc/vue/components.js
+++ dist/bc/vue/components.js
@@ -1,6 +1,6 @@
 /*! BC 平台的 vue 组件
  * @author dragon <rongjihuang@gmail.com>
- * @version v0.2.0-SNAPSHOT 2016-08-15
+ * @version v0.2.0 2016-08-15
  * @license Apache License 2.0
  * @components bc-theme
  *             bc-button
