177be418ec21e779e8b02c7a658c2d325678b353
dragon <rongjihuang@gmail.com>
2016-10-23 10:16:38 +0800

    auto copy result files to target dir for build.sh

--- tools/build.sh
+++ tools/build.sh
@@ -23,5 +23,17 @@ cp -f temp/$componentsFile dist/$componentsFile
 echo "build dist/bc/vue/components.min.js, dist/bc/vue/components.min.js.map"
 uglifyjs dist/$componentsFile -o dist/bc/vue/components.min.js --source-map dist/bc/vue/components.min.js.map -m -c --comments --source-map-url components.min.js.map
 
-echo "builded"
+# copy result files to target dir
+if [[ $1 ]]; then
+	if [[ ! -d $1 ]]; then
+		echo "target dir not exists, ignore copy result files to target dir."
+		exit 0;
+	fi
+	echo "copy result files to $1"
+	cp -f dist/bc/vue/components.js $1
+	cp -f dist/bc/vue/components.min.js $1
+	cp -f dist/bc/vue/components.min.js.map $1
+fi
+
+echo "build success."
 exit 0
