f91d2b71e8966e7323123056dfb5a028c0b14338
RJ.HWang <rongjihuang@gmail.com>
2017-11-09 15:23:50 +0800

    release 0.9.0

--- src/bc/vue/components.js
+++ src/bc/vue/components.js
@@ -1,6 +1,6 @@
 /*! BC 平台的 vue 组件
  * @author dragon <rongjihuang@gmail.com>
- * @version v0.8.2 2017-11-09
+ * @version v0.9.0 2017-11-13
  * @license Apache License 2.0
  * @components bc-theme
  *             bc-button
@@ -8,13 +8,15 @@
  *             bc-search
  *             bc-toolbar
  *             bc-table-col
- *             bc-page-bar
  *             bc-loading
+ *             bc-box-pointer
+ *             bc-page-bar
+ *             bc-page-bar-importer
  *             bc-grid
  */
 define(["bc/vue/theme", "bc/vue/button", "bc/vue/button-set", "bc/vue/search"
-	, "bc/vue/toolbar" , "bc/vue/box-pointer"
-	, "bc/vue/table-col", "bc/vue/page-bar", "bc/vue/loading"
+	, "bc/vue/toolbar" , "bc/vue/box-pointer", "bc/vue/loading"
+	, "bc/vue/table-col", "bc/vue/page-bar"
 	, "bc/vue/grid"
 ], function () {
 	return 0;
