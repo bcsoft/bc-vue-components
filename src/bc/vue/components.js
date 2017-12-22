b91b111435cd03d7466721d61b37ed7913899cac
RJ.Hwang <rongjihuang@gmail.com>
2017-12-22 16:43:51 +0800

    Release 0.10.4

--- src/bc/vue/components.js
+++ src/bc/vue/components.js
@@ -1,6 +1,6 @@
 /*! BC 平台的 vue 组件
  * @author dragon <rongjihuang@gmail.com>
- * @version v0.10.3 2017-12-17
+ * @version v0.10.4 2017-12-22
  * @license Apache License 2.0
  * @components bc-theme
  *             bc-button
